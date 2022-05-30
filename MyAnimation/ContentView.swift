b7e04f1cacd4345c75af5ec2a90c85d8fc585f2d
Artem <artem.kh4rchenko@ya.ru>
2022-05-30 14:15:31 +0700

    Initial Commit

---
+++ MyAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAnimation
+//
+//  Created by Артём Харченко on 30.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAnimation/MyAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAnimationApp.swift
+//  MyAnimation
+//
+//  Created by Артём Харченко on 30.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
